7f940bbb523e912ec6494f206267ed7762da05a5
Gabriel Lumertz <glumertz10@gmail.com>
2022-11-19 01:12:25 -0300

    API test

---
+++ backend/src/main/java/dev/lumertz/dsmeta/controllers/SaleController.java
@@ -0,0 +1,24 @@
+package dev.lumertz.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.lumertz.dsmeta.entities.Sale;
+import dev.lumertz.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public List<Sale> findSles() {
+		return service.findSales();
+	}
+}
